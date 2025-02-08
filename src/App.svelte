ec59018fc3b03d6f6dcf9afc6a1f5916dbe07a3f
boostybaboon <ilgardner@yahoo.co.uk>
2025-02-08 21:27:02 +0000

    refac of filters

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,29 @@
 <script lang="ts">
   import TasksForm from "./components/tasks-form.svelte";
   import TasksList from "./components/tasks-list.svelte";
-  import type { Task } from "./components/types";
+  import type { Task, Filter } from "./components/types";
 
   let message = "Tasks App";
+  let currentFilter = $state<Filter>("all");
   let tasks = $state<Task[]>([]);
   let totalDone = $derived(
-    tasks.reduce(
-      (total, task) => total + Number(task.done),
-      0
-    )
+    tasks.reduce((total, task) => total + Number(task.done), 0),
   );
+
+  let filteredTasks = $derived.by(() => {
+    switch (currentFilter) {
+      case "all": {
+        return tasks;
+      }
+      case "todo": {
+        return tasks.filter((t) => !t.done);
+      }
+      case "done": {
+        return tasks.filter((t) => t.done);
+      }
+    }
+    return tasks;
+  });
 
   function addTask(newTask: string) {
     tasks.push({
@@ -24,10 +37,19 @@
     task.done = !task.done;
   }
 
-  function removeTask(index: number) {
+  function removeTask(id: string) {
+    const index = tasks.findIndex((t) => t.id === id);
     tasks.splice(index, 1);
   }
 </script>
+
+{#snippet filterButton(filter: Filter)}
+  <button
+    onclick={() => (currentFilter = filter)}
+    class:contrast={currentFilter === filter}
+    class="secondary filterButton">{filter}</button
+  >
+{/snippet}
 
 <main>
   <h1>{message}</h1>
@@ -39,12 +61,28 @@
       Add a task to get started!
     {/if}
   </p>
-  <TasksList {tasks} {toggleDone} {removeTask} />
+  {#if tasks.length}
+    <div class="button-container">
+      {@render filterButton("all")}
+      {@render filterButton("todo")}
+      {@render filterButton("done")}
+    </div>
+  {/if}
+  <TasksList tasks={filteredTasks} {toggleDone} {removeTask} />
 </main>
 
 <style>
   main {
     margin: 1rem auto;
     max-width: 800px;
+  }
+  .button-container {
+    display: flex;
+    justify-content: end;
+    margin-bottom: 1rem;
+    gap: 0.5rem;
+  }
+  .filterButton {
+    text-transform: capitalize;
   }
 </style>
