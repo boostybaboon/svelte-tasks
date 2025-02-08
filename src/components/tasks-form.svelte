0a7f041adf04a8a23bcd5469cf026c587610d75d
boostybaboon <ilgardner@yahoo.co.uk>
2025-02-08 20:08:12 +0000

    styling and cleaning to match youtube tutorial

---
+++ src/components/tasks-form.svelte
@@ -0,0 +1,15 @@
+<form>
+    <label>
+        <input name="newTask" />
+    </label>
+    <div class="button-container">
+        <button>Add</button>
+    </div>
+</form>
+
+<style>
+    .button-container {
+        display: flex;
+        justify-content: end;
+    }
+</style>
